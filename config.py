117fadd130d891885647574b1caa4b160e78e379
jasonmc02gap <jmelendez@growthaccelerationpartners.com>
2018-02-08 17:06:33 -0600

    c2

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_SECRET_KEY = 'super-secret-key'
